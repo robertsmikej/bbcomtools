1458aefc6f1f1a26f59a6befb61cb34cb23c7470
Michael <michael@robertsmj.com>
2020-05-28 15:27:01 -0600

    add

--- plugins/global.js
+++ plugins/global.js
@@ -1,28 +1,28 @@
 import Vue from 'vue';
 
-import Banners from '~/components/Banners.vue';
+import Banners from '../components/Banners.vue';
 Vue.component('Banners', Banners);
 
-import Headers from '~/components/Headers.vue';
+import Headers from '../components/Headers.vue';
 Vue.component('Headers', Headers);
 
-import List from '~/components/List.vue';
+import List from '../components/List.vue';
 Vue.component('List', List);
 
-import Nav from '~/components/Nav.vue';
+import Nav from '../components/Nav.vue';
 Vue.component('Nav', Nav);
 
-import Optionsbuttons from '~/components/Optionsbuttons.vue';
+import Optionsbuttons from '../components/Optionsbuttons.vue';
 Vue.component('Optionsbuttons', Optionsbuttons);
 
-import Paragraphs from '~/components/Paragraphs.vue';
+import Paragraphs from '../components/Paragraphs.vue';
 Vue.component('Paragraphs', Paragraphs);
 
-import Sidebyside from '~/components/Sidebyside.vue';
+import Sidebyside from '../components/Sidebyside.vue';
 Vue.component('Sidebyside', Sidebyside);
 
-import Spacer from '~/components/Spacer.vue';
+import Spacer from '../components/Spacer.vue';
 Vue.component('Spacer', Spacer);
 
-import Videos from '~/components/Videos.vue';
+import Videos from '../components/Videos.vue';
 Vue.component('Videos', Videos);
