602ac137b3850fa233b6cef7b47e4d606e2de461
Michael <michael@robertsmj.com>
2020-05-28 11:16:06 -0600

    add

--- plugins/global.js
+++ plugins/global.js
@@ -1,32 +1,32 @@
 import Vue from 'vue';
 
-import Nav from '../components/Nav.vue';
+import Nav from '~/components/Nav.vue';
 Vue.component('Nav', Nav);
 
-import OptionsButtons from '../components/OptionsButtons.vue';
+import OptionsButtons from '~/components/OptionsButtons.vue';
 Vue.component('OptionsButtons', OptionsButtons);
 
-import TextInput from '../components/inputs/TextInput.vue';
+import TextInput from '~/components/inputs/TextInput.vue';
 Vue.component('TextInput', TextInput);
 
 
-import Headers from '../components/Headers.vue';
+import Headers from '~/components/Headers.vue';
 Vue.component('Headers', Headers);
 
-import Paragraphs from '../components/Paragraphs.vue';
+import Paragraphs from '~/components/Paragraphs.vue';
 Vue.component('Paragraphs', Paragraphs);
 
-import List from '../components/List.vue';
+import List from '~/components/List.vue';
 Vue.component('List', List);
 
-import Spacer from '../components/Spacer.vue';
+import Spacer from '~/components/Spacer.vue';
 Vue.component('Spacer', Spacer);
 
-import SideBySide from '../components/SideBySide.vue';
+import SideBySide from '~/components/SideBySide.vue';
 Vue.component('SideBySide', SideBySide);
 
-import Videos from '../components/Videos.vue';
+import Videos from '~/components/Videos.vue';
 Vue.component('Videos', Videos);
 
-import Banners from '../components/Banners.vue';
+import Banners from '~/components/Banners.vue';
 Vue.component('Banners', Banners);
