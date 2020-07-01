f60e990ae17418ed9df9499ae42e567adb77d823
benkirbycodes <benkirbycodes@gmail.com>
2020-07-01 16:49:27 -0600

    Chart v-for added

--- components/pagecomponents/Chart.vue
+++ components/pagecomponents/Chart.vue
@@ -1,12 +1,31 @@
 <template>
   <div class="page__component">
-    <div></div>
+    <div v-for="(chartRow, index) in chartRows" 
+    :key="componentData.uniqueName + index"
+    >
+    </div>
   </div>
 </template>
 
 <script>
 export default {
-
+    props: {
+        type: String,
+        componentData: Object,
+        group: Boolean,
+        subgroup: Boolean,
+        parentData: Object,
+        items: Object
+    },
+    computed: {
+      chartRows() {
+                    if (this.items) {
+                return this.items.elementData.chartRows;
+            } else {
+                return this.componentData.elementData.chartRows;
+            }
+      }
+    }
 }
 </script>
 
